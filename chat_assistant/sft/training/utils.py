0e507fd3e51c67389be8d83fd3335ca461a43b7c
shah-unwindhomes <95495154+shah-unwindhomes@users.noreply.github.com>
2024-03-23 13:55:06 -0700

    no eval dataset

--- chat_assistant/sft/training/utils.py
+++ chat_assistant/sft/training/utils.py
@@ -90,7 +90,10 @@ def create_datasets(tokenizer, data_args, training_args, apply_chat_template=Fal
         )
 
     train_data = raw_datasets["train"]
-    valid_data = raw_datasets["test"]
+    valid_data = None
+    if "test" in raw_datasets:
+        valid_data = raw_datasets["test"]
+
     print(
         f"Size of the train set: {len(train_data)}. Size of the validation set: {len(valid_data)}"
     )
